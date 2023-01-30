1d939cd58611fa436ba3a1384c20163aeabc377a
Daniyarbek1 <123979615+Daniyarbek1@users.noreply.github.com>
2023-01-31 00:17:34 +0600

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,12 @@
+fullname = "Artybayev Daniyarbek Zhandarbekovich"
+university ="satbayev university"
+course = 3
+study_days=7
+day = int (input('vvedite den nedeli:'))
+if(day%2==0):
+    print("Daniyarbek idet v univer")
+
+else :
+    print("Daniyarbek ne idet v univer")
+
+#tut sdelal fio vse klassno
